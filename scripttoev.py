2475f73db3d635207ad7510b64ba6419350c09ee
Maya Islambouli <43270477+MayaIsla@users.noreply.github.com>
2023-08-23 14:55:45 -0500

    Update scripttoev.py

--- scripttoev.py
+++ scripttoev.py
@@ -9,7 +9,7 @@ dict = "C:/Users/misl7603/Desktop/Scripts/Address Cleanup/sample.csv"
 df = pd.read_csv(dict)
 
 for i in df['Address']:
-    resp = requests.get('https://api.geoapify.com/v1/geocode/search', params= {'text': i, 'apiKey': '45b2db56d0bc42ab988a9329d451119f'} )
+    resp = requests.get('https://api.geoapify.com/v1/geocode/search', params= {'text': i, 'apiKey': 'KEY'} )
     data = resp.json()
 
     for properties in data['features']:
@@ -18,7 +18,7 @@ for i in df['Address']:
 
 converted = pd.DataFrame(eval(addresses))
 #h1 = pd.DataFrame(addresses)
-#h1
+
 
     
 
